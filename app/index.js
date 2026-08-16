45888dedabc2a4811a1c590c880857b8237a6ba7
Sneha Jana <snehajana461@gmail.com>
2026-08-16 22:45:52 +0530

    feat: add health check endpoint

--- app/index.js
+++ app/index.js
@@ -3,12 +3,35 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
+  res.setHeader("Content-Type", "application/json");
+
+  if (req.url === "/health") {
+    res.writeHead(200);
+    res.end(
+      JSON.stringify({
+        status: "healthy",
+        service: "devops-capstone-app",
+      }),
+    );
+    return;
+  }
+
+  if (req.url === "/") {
+    res.writeHead(200);
+    res.end(
+      JSON.stringify({
+        message: "🚀 DevOps Capstone Project Running Successfully on AWS!",
+        status: "healthy",
+        timestamp: new Date().toISOString(),
+      }),
+    );
+    return;
+  }
+
+  res.writeHead(404);
   res.end(
     JSON.stringify({
-      message: "🚀 DevOps Capstone Project Running Successfully on AWS!",
-      timestamp: new Date().toISOString(),
-      status: "Healthy",
+      error: "Route not found",
     }),
   );
 });
